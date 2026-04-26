47b7b97bcb30d2a06cbf5a76f3f97285e394f962
kNiga <thinkandact@yandex.ru>
2026-04-26 17:38:27 +0300

    Update integration tests

--- src/test/java/com/radosti/app/integration/integrationTest.java
+++ src/test/java/com/radosti/app/integration/integrationTest.java
@@ -13,7 +13,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class integrationTest {
     @Test
-    void TestRegisterReserveAndList(){
+    void Test_ApartmentRegistation_And_MakingList(){
+        ClientService clientService = new ClientService();
+        ApartmentService apartmentService = new ApartmentService(clientService);
+
+        ApartmentRegister ApRegister = new ApartmentRegister(apartmentService);
+
+        String[] ap_register = {"apartment", "register", "1", "100.0"};
+
+        ApRegister.execute(ap_register);
+
+        List<Apartment> list = apartmentService.listApartments(1, 1, "price");
+
+        assertEquals(1,list.size());
+        assertEquals(100.0,list.get(0).getPrice());
+    }
+    @Test
+    void Test_ApartmentRegistration_ClientRegistration_ApartmentReserve(){
+        ClientService clientService = new ClientService();
+        ApartmentService apartmentService = new ApartmentService(clientService);
+
+        ApartmentRegister ApRegister = new ApartmentRegister(apartmentService);
+        ClientRegister clientRegister = new ClientRegister(clientService);
+        ApartmentReserve ApReserve = new ApartmentReserve(apartmentService);
+
+        String[] ap_register = {"apartment", "register", "1", "100.0"};
+        String[] client_register = {"client", "register", "AB123", "John", "Johnson"};
+        String[] ap_reserve = {"apartment", "reserve", "1", "AB123"};
+
+        ApRegister.execute(ap_register);
+        clientRegister.execute(client_register);
+        ApReserve.execute(ap_reserve);
+
+    }
+    @Test
+    void Test_ApartmentRegistration_ClientRegistration_ApartmentReserve_MakingList(){
         ClientService clientService = new ClientService();
         ApartmentService apartmentService = new ApartmentService(clientService);
 
